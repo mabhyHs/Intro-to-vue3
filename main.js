63f8ffa71b3befc30cc2812b12368705d56f70be
Mabhy <m4huens@gmail.com>
2021-12-14 15:19:49 -0300

    addingCartAndButton

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product : 'socks',
             image: './assets/images/socks_green.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants:[
-                {id:2234, color: 'green'},
-                {id:2235, color: 'blue'},
-            ]
-                }
+                {id:2234, color: 'green', image:'./assets/images/socks_green.jpg'},
+                {id:2235, color: 'blue', image:'./assets/images/socks_blue.jpg'},
+             ]
+         }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1; //aumenta el valor de uno en uno
+        },
+        updateImage(variantImage){
+            this.image = variantImage;
+        }
+
     }
 })
